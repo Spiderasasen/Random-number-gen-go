14d7411f4ee62e87944e3686b11960ec09ee5430
spiderasasen <spiderasasen@gmail.com>
2026-04-16 08:55:37 -0400

    learning the new syntax of go

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"math/rand"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	//getting the random number
+	var randNum int = random_number() //this is a var in go, var <name> <operation>
+	println(randNum)
+}
+
+// getting a random number
+func random_number() int { //must put the return statment at the end of the function line
+	rand.Seed(time.Now().UnixNano())
+	num := rand.Intn(10)
+	return num
 }
