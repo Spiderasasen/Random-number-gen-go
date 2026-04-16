276572016b7e8007c44622c9ab5ccab56d0c3578
spiderasasen <spiderasasen@gmail.com>
2026-04-16 09:15:40 -0400

    finished

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"math/rand"
 	"time"
 )
@@ -8,12 +9,34 @@ import (
 func main() {
 	//getting the random number
 	var randNum int = random_number() //this is a var in go, var <name> <operation>
-	println(randNum)
+
+	var choice int
+
+	for {
+		println("Please enter a random number: ")
+		_, err := fmt.Scan(&choice) //_ tells to forget, err is the error handler (no try expect here)
+
+		//checks if the number is a number
+		if err != nil {
+			fmt.Println("Please enter a number")
+			continue
+		}
+
+		//checking if the user got the number, is too low or too high
+		if choice == randNum {
+			fmt.Println("You Win!!!")
+			break
+		} else if choice < randNum { //this is discusting but ok
+			fmt.Println("To low")
+		} else { //as for this as well
+			fmt.Println("To high")
+		}
+	}
 }
 
 // getting a random number
 func random_number() int { //must put the return statment at the end of the function line
-	rand.Seed(time.Now().UnixNano())
+	rand.Seed(time.Now().UnixNano()) //seed of the random number (should be in main, but im learning)
 	num := rand.Intn(10)
 	return num
 }
